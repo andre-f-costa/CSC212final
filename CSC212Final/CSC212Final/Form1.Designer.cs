0715846ebd1873d30ad84f6af872e55dfbe43805
Andre Costa <andre_costa@uri.edu>
2021-12-07 21:32:26 -0500

    Finalized

--- CSC212Final/CSC212Final/Form1.Designer.cs
+++ CSC212Final/CSC212Final/Form1.Designer.cs
@@ -47,10 +47,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1400, 1033);
-            this.button1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button1.Location = new System.Drawing.Point(449, 283);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(238, 65);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,30 +57,27 @@
             // 
             // txt_output
             // 
-            this.txt_output.Location = new System.Drawing.Point(142, 219);
-            this.txt_output.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txt_output.Location = new System.Drawing.Point(45, 77);
             this.txt_output.Name = "txt_output";
-            this.txt_output.Size = new System.Drawing.Size(1508, 44);
+            this.txt_output.Size = new System.Drawing.Size(479, 20);
             this.txt_output.TabIndex = 1;
             this.txt_output.TextChanged += new System.EventHandler(this.txt_output_TextChanged);
             // 
             // txt_iterations
             // 
             this.txt_iterations.ForeColor = System.Drawing.Color.Black;
-            this.txt_iterations.Location = new System.Drawing.Point(142, 641);
-            this.txt_iterations.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txt_iterations.Location = new System.Drawing.Point(45, 225);
             this.txt_iterations.Name = "txt_iterations";
-            this.txt_iterations.Size = new System.Drawing.Size(371, 44);
+            this.txt_iterations.Size = new System.Drawing.Size(120, 20);
             this.txt_iterations.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(133, 174);
-            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Location = new System.Drawing.Point(42, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(305, 37);
+            this.label1.Size = new System.Drawing.Size(102, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "File Output Location";
             // 
@@ -89,10 +85,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(133, 595);
-            this.label2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label2.Location = new System.Drawing.Point(42, 209);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(290, 37);
+            this.label2.Size = new System.Drawing.Size(96, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Numer of Iterations";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -101,10 +96,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(133, 315);
-            this.label3.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label3.Location = new System.Drawing.Point(42, 111);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(184, 37);
+            this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Image Type";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -114,10 +108,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(127, 57);
-            this.label4.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label4.Location = new System.Drawing.Point(40, 20);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(848, 72);
+            this.label4.Size = new System.Drawing.Size(306, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Recursive Imaging CSC 212";
             // 
@@ -125,10 +118,9 @@
             // 
             this.btn_hil.AutoSize = true;
             this.btn_hil.ForeColor = System.Drawing.Color.White;
-            this.btn_hil.Location = new System.Drawing.Point(142, 361);
-            this.btn_hil.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btn_hil.Location = new System.Drawing.Point(45, 127);
             this.btn_hil.Name = "btn_hil";
-            this.btn_hil.Size = new System.Drawing.Size(246, 41);
+            this.btn_hil.Size = new System.Drawing.Size(86, 17);
             this.btn_hil.TabIndex = 8;
             this.btn_hil.TabStop = true;
             this.btn_hil.Text = "Hilbert Curve";
@@ -139,10 +131,9 @@
             // 
             this.btn_ser.AutoSize = true;
             this.btn_ser.ForeColor = System.Drawing.Color.White;
-            this.btn_ser.Location = new System.Drawing.Point(142, 426);
-            this.btn_ser.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btn_ser.Location = new System.Drawing.Point(45, 150);
             this.btn_ser.Name = "btn_ser";
-            this.btn_ser.Size = new System.Drawing.Size(324, 41);
+            this.btn_ser.Size = new System.Drawing.Size(111, 17);
             this.btn_ser.TabIndex = 9;
             this.btn_ser.TabStop = true;
             this.btn_ser.Text = "Sierpinski Triangle";
@@ -151,10 +142,9 @@
             // 
             // txt_size
             // 
-            this.txt_size.Location = new System.Drawing.Point(142, 806);
-            this.txt_size.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txt_size.Location = new System.Drawing.Point(45, 283);
             this.txt_size.Name = "txt_size";
-            this.txt_size.Size = new System.Drawing.Size(308, 44);
+            this.txt_size.Size = new System.Drawing.Size(100, 20);
             this.txt_size.TabIndex = 11;
             this.txt_size.TextChanged += new System.EventHandler(this.txt_size_TextChanged);
             // 
@@ -162,10 +152,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(133, 763);
-            this.label5.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label5.Location = new System.Drawing.Point(42, 268);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(220, 37);
+            this.label5.Size = new System.Drawing.Size(74, 13);
             this.label5.TabIndex = 12;
             this.label5.Text = "Size of Output";
             // 
@@ -174,10 +163,9 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(706, 171);
-            this.label7.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label7.Location = new System.Drawing.Point(223, 60);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(945, 38);
+            this.label7.Size = new System.Drawing.Size(309, 13);
             this.label7.TabIndex = 15;
             this.label7.Text = "Please input file location following Linux format starting with /mnt";
             // 
@@ -186,10 +174,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(1479, 1144);
-            this.label8.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label8.Location = new System.Drawing.Point(470, 326);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(291, 31);
+            this.label8.Size = new System.Drawing.Size(99, 12);
             this.label8.TabIndex = 16;
             this.label8.Text = "André Costa, Fall 2021";
             // 
@@ -197,10 +184,9 @@
             // 
             this.btn_carp.AutoSize = true;
             this.btn_carp.ForeColor = System.Drawing.Color.White;
-            this.btn_carp.Location = new System.Drawing.Point(142, 486);
-            this.btn_carp.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btn_carp.Location = new System.Drawing.Point(45, 171);
             this.btn_carp.Name = "btn_carp";
-            this.btn_carp.Size = new System.Drawing.Size(304, 41);
+            this.btn_carp.Size = new System.Drawing.Size(104, 17);
             this.btn_carp.TabIndex = 17;
             this.btn_carp.TabStop = true;
             this.btn_carp.Text = "Sierpinski Carpet";
@@ -208,10 +194,10 @@
             // 
             // RecursiveImaging
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Navy;
-            this.ClientSize = new System.Drawing.Size(1830, 1235);
+            this.ClientSize = new System.Drawing.Size(581, 347);
             this.Controls.Add(this.btn_carp);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -227,7 +213,6 @@
             this.Controls.Add(this.txt_output);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "RecursiveImaging";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Recursive Imaging";
